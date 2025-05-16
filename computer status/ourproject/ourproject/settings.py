e11865d107af5d37f82c0800f55dbbabede683fc
zhairajanemartinez <zhairajanemartinez@gmail.com>
2025-05-16 11:02:16 +0900

    Add files via upload

--- computer status/ourproject/ourproject/settings.py
+++ computer status/ourproject/ourproject/settings.py
@@ -128,3 +128,5 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = '/admin/login'
